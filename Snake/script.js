e6dbf714d1d0f0ae8db8511d5fa54d4188edae07
John Doe <johndoe@example.com>
2024-06-22 00:02:28 +0700

    Змейка двигается

--- Snake/script.js
+++ Snake/script.js
@@ -10,6 +10,7 @@ for (let y = 1; y <= 20; y++){
         ground.appendChild(cell);
     }
 }
+
 function render(snake){
     for (let y = 1; y <= 20; y++){
         for(let x = 1; x <= 20; x++){
@@ -24,14 +25,53 @@ function render(snake){
         }
     }
 }
-function Game(snake, status){
-    while (status){
-        if (status == 0) return 0;
-        snake.pop[-1];
-        if (snake.length == 0) status = 0;
+
+function Game(snake, status, id, direction){
+    let x_first = Number(snake[0].split(' ')[0]);
+    let y_first = Number(snake[0].split(' ')[1]);
+    snake.pop();
+    if (direction == 'down'){
+        snake.unshift(x_first + ' ' + (y_first + 1));
+        direction_status = 0;
     }
+    else if (direction == 'up'){
+        snake.unshift(x_first + ' ' + (y_first - 1));
+        direction_status = 0;
+    }
+    else if (direction == 'right'){
+        snake.unshift((x_first + 1) + ' ' + y_first);
+        direction_status = 1;
+    }
+    else if (direction == 'left'){
+        snake.unshift((x_first - 1) + ' ' + y_first);
+        direction_status = 1;
+    }
+    x_first = Number(snake[0].split(' ')[0]);
+    y_first = Number(snake[0].split(' ')[1]);
+    if (x_first < 1 || x_first > 20 || y_first < 1 || y_first > 20 ) status = 0;
+    if (status == 0) {
+        clearInterval(id);
+        return 0;
+    }
+    render(snake);
 }
+
 let snake = ['1 3', '1 2', '1 1'];
 render(snake);
-let status = 1;
-Game(snake, status);
+let direction = 'down';
+let direction_status = 0;
+let id = setInterval(()=> {Game(snake, 1, id, direction)},500);
+document.addEventListener("keydown", function(event){
+    if (event.code == "ArrowRight" && direction_status == 0) {
+        direction = 'right';
+    }
+    else if (event.code == "ArrowLeft" && direction_status == 0) {
+        direction = 'left';
+    }
+    else if (event.code == "ArrowUp" && direction_status == 1) {
+        direction = 'up';
+    }
+    else if (event.code == "ArrowDown" && direction_status == 1) {
+        direction = 'down';
+    }
+})
